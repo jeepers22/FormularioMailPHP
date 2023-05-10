622199e322cc71c2c364a6be81cb835bc5a443b7
Maximiliano Safran <maxizero@gmail.com>
2023-05-10 19:48:46 -0300

    Cambiosss

--- form.php
+++ form.php
@@ -4,15 +4,20 @@ $nombre = $_POST['name'];
 $mail = $_POST['email'];
 $mensaje = $_POST['abstract'];
 
+$header = 'From: ' . $mail . " \r\n";
+$header .= 'X-Mailer:PHP/' . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
 $mensajeMail = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensajeMail .= "Su e-mail es: " . $mail . " \r\n";
-$mensajeMail .= "Mensaje: " . $mensaje . " \r\n";
+$mensajeMail .= "Mensaje: " . $_POST['abstract'] . " \r\n";
 $mensajeMail .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'maxizero@gmail.com';
+$para = $mail;
 $asunto = 'Este mail fue enviado desde la web';
 
-mail($para, $asunto, utf8_decode($mensaje), $header);
+mail($para, $asunto, utf8_decode($mensajeMail), $header);
 
 header('Location:exito.html');
 
